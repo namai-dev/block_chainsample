c3dd51adc91ea10d01472ebf8de3b3d8405f42da
namai-dev <namaiHarmony4@gmail.com>
2023-11-24 08:55:05 +0300

    Block

--- blockchain.py
+++ blockchain.py
@@ -5,7 +5,7 @@ import json as _json
 class BlockChain:
     def __init__(self) -> None:
         self.chain = list()
-        self.block = None
+        self.genesis_block = self.create_block(index=1, proof_of_work=1, previous_hash=1, data="Genesis Block")
     
     def create_block(self, index: int,proof_of_work: int, previous_hash:str, data: str) -> dict:
          block = {
@@ -18,5 +18,7 @@ class BlockChain:
         }
          
          return block
+    
+
 
         
